9fc9b5ac2bd03e8165df3e99e92d4c68fc4489e0
kaique coimbra <kaique.caires@yahoo.com>
2020-12-28 09:14:01 -0300

    chore: update jest-config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
