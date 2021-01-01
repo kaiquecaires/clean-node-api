835539b046c0c7ffae4581ddd7e512a44e529032
kaique coimbra <kaique.caires@yahoo.com>
2021-01-01 20:26:34 -0300

    test: ensure EmailValidator returns true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,4 +7,10 @@ describe('Email validator Adapter', () => {
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
+
+  test('should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
 })
