7722240834fc2685d2fd6de0d0d6fb8f96d04872
kaique coimbra <kaique.caires@yahoo.com>
2021-01-10 19:26:25 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/useCases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptograph/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return signUpController
+}
