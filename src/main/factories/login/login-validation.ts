894d6dc7bd52a3d2a6b9987eb2cd23e8733e5baf
kaiquecaires <kaique.caires@yahoo.com>
2021-02-16 13:53:50 -0300

    feat: add LoginValidationFactory

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,16 @@
+import { RequiredFielValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+
+export const makeLoginValidation = (): Validation => {
+  const validations: Validation[] = []
+  for (const field of ['email', 'password']) {
+    validations.push(new RequiredFielValidation(field))
+  }
+
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+
+  return new ValidationComposite(validations)
+}
