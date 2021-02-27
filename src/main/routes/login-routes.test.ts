74d61bafd1f7ef4f26f5878af15d90fa6e11420a
kaiquecaires <kaique.caires@yahoo.com>
2021-02-27 15:11:39 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
