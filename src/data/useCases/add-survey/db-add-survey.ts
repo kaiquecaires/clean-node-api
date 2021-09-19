b42363424e57dc30bcb9e28b66c82c3cde38e7c4
Kaique Caires <kaique.caires@yahoo.com>
2021-09-19 08:46:30 -0300

    test: ensure DbLoadAccountByToken calls Decrypter with correct value

--- src/data/useCases/add-survey/db-add-survey.ts
+++ src/data/useCases/add-survey/db-add-survey.ts
@@ -1,8 +1,8 @@
-import { AddSurveyRepository } from '../../protocols/db/survey/add-suvey-repository';
+import { AddSurveyRepository } from '../../protocols/db/survey/add-suvey-repository'
 import { AddSurvey, AddSurveyModel } from './db-add-survey-protocols'
 
 export class DbAddSurvey implements AddSurvey {
-  constructor(private readonly addSurveyRepository: AddSurveyRepository) {}
+  constructor (private readonly addSurveyRepository: AddSurveyRepository) {}
 
   async add (data: AddSurveyModel): Promise<void> {
     await this.addSurveyRepository.add(data)
