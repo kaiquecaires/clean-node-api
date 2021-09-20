0577f2dc6a59696ea95b9b0e77a93623c32b2def
Kaique Caires <kaique.caires@yahoo.com>
2021-09-20 12:33:10 -0300

    feat: ensure add survey route returns 403 if no access token is provided

---
+++ src/main/factories/middlewares/auth-middleware-factory.ts
@@ -0,0 +1,7 @@
+import { AuthMiddleware } from '../../../presentation/middlewares/auth-middleware'
+import { Middleware } from '../../../presentation/protocols'
+import { makeDbLoadAccount } from '../useCases/account/load-account-by-token.ts/db-load-account-by-token-factory'
+
+export const makeAuthMiddleware = (role?: string): Middleware => {
+  return new AuthMiddleware(makeDbLoadAccount(), role)
+}
