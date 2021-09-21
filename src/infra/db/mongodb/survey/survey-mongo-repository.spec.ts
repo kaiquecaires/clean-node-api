ba20b63e00e546e8fae5af971c266414bf60e93e
Kaique Caires <kaique.caires@yahoo.com>
2021-09-21 12:15:34 -0300

    refactor: organize SurveyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -25,22 +25,24 @@ describe('Survey Mongo Repository', () => {
     return new SurveyMongoRepository()
   }
 
-  test('Should return a survey on success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [
-        {
-          answer: 'any_answer',
-          image: 'any_image'
-        },
-        {
-          answer: 'other_answer'
-        }
-      ],
-      date: new Date()
+  describe('add()', () => {
+    test('Should return a survey on success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [
+          {
+            answer: 'any_answer',
+            image: 'any_image'
+          },
+          {
+            answer: 'other_answer'
+          }
+        ],
+        date: new Date()
+      })
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
   })
 })
