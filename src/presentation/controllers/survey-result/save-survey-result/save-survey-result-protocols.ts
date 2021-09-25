66f07953bb2ed39e864e9c986d38bdd62ad5ca01
Kaique Caires <kaique.caires@yahoo.com>
2021-09-25 18:54:47 -0300

    feat: ensure SaveSurveyController calls LoadSurveyById with correct values

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-protocols.ts
@@ -0,0 +1,4 @@
+export * from '@/presentation/protocols'
+export * from '@/domain/useCases/survey/load-survey-by-id'
+export * from '@/domain/models/survey'
+export * from '@/domain/useCases/survey/load-survey-by-id'
