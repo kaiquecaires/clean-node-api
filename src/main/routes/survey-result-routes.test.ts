977f4e150ec93d723a4da91044310b9d74aa5a29
Kaique Caires <kaique.caires@yahoo.com>
2021-10-04 16:24:56 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -90,5 +90,28 @@ describe('Survey Result Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          },
+          {
+            answer: 'Answer 2',
+            image: 'http://image-name.com'
+          }
+        ],
+        date: new Date()
+      })
+
+      await request(app)
+        .get(`/api/surveys/${String(res.ops[0]._id)}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
