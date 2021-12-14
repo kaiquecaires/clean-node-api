fd5b30ea45045da021bb4c8b2283e35008a443bc
kaique caires <kaique.caires@yahoo.com>
2021-12-14 13:35:01 -0300

    feat: setup apollo server

---
+++ src/main/config/apollo-server.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from 'apollo-server-express'
+import { Express } from 'express'
+import typeDefs from '../graphql/type-defs'
+import resolvers from '../graphql/resolvers'
+
+export default async (app: Express): Promise<void> => {
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs
+  })
+
+  await server.start()
+
+  server.applyMiddleware({ app })
+}
--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import express from 'express'
-import setupSwagger from './config-swagger'
+import setupSwagger from './swagger'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
+import setupApolloServer from './apollo-server'
 
 const app = express()
 setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
+setupApolloServer(app)
 export default app
